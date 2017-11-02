da0d700ddeabee2c1390716b5e2ccce957e4c341
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-11-02 16:02:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === stopValue) {
       break
-    } else {
+    }
       array[i] = changeValue
     }
   }
