bf2ceccbeefa6f8beae7afb45993c5d282d91b2f
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-11-02 16:06:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-function breakOut(array, changeValue, stopValue) {
-  for (let i = 0; i < array.length; i++) {
-    array[i] = changeValue;
-    if (array[i] === stopValue) {
-      break;
+  function breakOut(array, changeValue, stopValue) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === stopValue) {
+        break;
+      }
+      array[i] = changeValue;
     }
+
+    return array;
   }
-
-  return array;
-}
-
-  // function breakOut(array, changeValue, stopValue) {
-  //   for (let i = 0; i < array.length; i++) {
-  //     if (array[i] === stopValue) {
-  //       break;
-  //     }
-  //       array[i] = changeValue;
-  //     }
-  //
-  //     return array;
-  //   }
