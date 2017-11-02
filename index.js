d92e9bfa2002fe558827efa784885eee37dcd8ff
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-11-02 16:17:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
-    if (findFn(array[i])) {
+    if (findFn(array[i]) === array[i]) {
       array[i];
     }
 
